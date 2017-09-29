f707f15a9234b23c604aa3e090198869ba3aea18
jprietope <jprietope@gmail.com>
2017-09-29 10:52:11 +0200

    Agregar archivos de proyecto.

---
+++ ProyDYNAMIC/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyDYNAMIC.Startup))]
+namespace ProyDYNAMIC
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
